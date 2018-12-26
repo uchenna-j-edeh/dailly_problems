7d912ee8bd893a324a1e9860159ea738d8945cdc
Uchenna J. Edeh <uchenna.j.edeh@jpmchase.com>
2018-12-26 12:29:36 -0600

    array_time_intervals.py is not yet working as expected...

--- array_time_intervals.py
+++ array_time_intervals.py
@@ -9,18 +9,34 @@ count overlap as 0 and count non overlap as 2
 """
 import sys
 def solution1(my_list):
-    #print (list_tuples)
+    counter = 0 
+    seenit = []
     for i, _list in enumerate(my_list):
-        x, y = int(_list[0]), int(_list[1])
-        for k in my_list[i+1:]: 
+    #    x, y = int(_list[0]), int(_list[1])
+#        import pdb; pdb.set_trace()
+        for k in range(i+1, len(my_list)): 
+            if is_overlapped(_list, my_list[k]):
+                if my_list[k] not in seenit:
+                    counter = counter + 2 # any overlap reduces the max possible time slots by 1
 
+                seenit.append(my_list[k])
 
+    return counter
+
+def is_overlapped(first_array, second_array):
+    x, y = int(first_array[0]), int(first_array[1])
+    m, n = int(second_array[0]), int(second_array[1])
+
+    if x >= m and x <= n or m >= x and m <= y:
+        return True
+
+    return False
 
 def main(args):
     if len(args) != 2:
         raise AssertionError("Usage:\n\tpython3 {0} '{1}'\n\tExpected Result: {2}\n\tPlease Try Again!\n\t".format(__file__, "30,75;0,50;60,150", '2' ))
     my_list = [x.split(',') for x in args[1].split(';')]
-    solution1(my_list)
+    print(solution1(my_list))
 
 if __name__ == "__main__":
     try:
