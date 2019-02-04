659e44c38393e92ae569445eb8bc2f5e3c6469b4
Uchenna J Edeh <write2uche@gmail.com>
2019-02-04 21:20:25 +0100

    added to the previous solution of counting_sort_diff.py, reverse_half_array.py ...

--- counting_sort_diff.py
+++ counting_sort_diff.py
@@ -16,27 +16,30 @@ def solution1(my_nums):
         if i < my_min:
             my_min = i
 
-    start_point = 0
-    if my_min < 0:
-        start_point = my_min
+#    start_point = 0
+#    if my_min < 0:
+#        start_point = my_min
 
-    int_seq = [0 for i in range(my_max+1)]
+    int_seq = [0 for i in range(my_min, my_max+1)]
     print(int_seq)
 
-    
+    # updating the int_seq with the frequecy of numbers 
     for k in my_nums:
-        int_seq[k] = int_seq[k] + 1
+        idx = k - my_min
+        int_seq[idx] = int_seq[idx] + 1
 
     print(int_seq)
     new_nums = []
     for j, val in enumerate(int_seq):
+        #m = j + my_min
         if val:
+            m = j + my_min
             for n in range(val):
-                new_nums.append(j)
+                new_nums.append(m)
 
     print(new_nums)
 
-nums = '21,41,17,45,9,28'
+nums = '21,41,17,45,9,28,-10,9'
 
 print(solution1([int(x) for x in nums.split(',')]))
 
--- reverse_half_array.py
+++ reverse_half_array.py
@@ -5,10 +5,13 @@ Example: 'abcdefghijklmnopqrstuvwxyz' would be 'abcdefghijklmnzyxwvutsrqpo'
 
 def solution1(text):
     len_list = len(text)//2
-    print(len_list)
-    quater = len(text[:len_list])//2
-    print(text[quater])
-    print(text[len_list])
+    #print(text[:len_list])
+    #print(text[len_list:])
+    
+    new_str = ''
+    for tx in text[len_list:]:
+        new_str = tx + new_str
 
+    return text[:len_list] + new_str
 
-print(solution1('abcdefghijklmnopqrstuvwxyz'))
+print(solution1('abcdefghijklmzyxwvutsrqpon'))
