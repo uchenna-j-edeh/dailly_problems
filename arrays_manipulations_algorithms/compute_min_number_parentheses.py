c74682c10acd00115bd00b5189918b24cbe0a4d7
Uchenna J. Edeh <uchenna.j.edeh@jpmchase.com>
2019-02-19 12:12:49 -0600

    compute_min_number_parentheses.py

---
+++ arrays_manipulations_algorithms/compute_min_number_parentheses.py
@@ -0,0 +1,29 @@
+"""
+Given a string of parentheses, write a function to compute the minimum number of
+parentheses to be removed to make the string valid (i.e each open parentheses is 
+eventually closed)
+
+For example, given the string "()())()", you should return 1. Given the string ")(",
+you should return 2, since we must remove all of them
+"""
+
+def solution(my_str):
+    top_char = ''
+    stack = []
+    
+    for i in my_str:
+        if top_char == '(' and i == ')':
+            stack.pop()
+            if len(stack)> 0:
+                top_char = stack[-1]
+            else:
+                top_char = ''
+        else:
+            stack.append(i)
+            top_char = i
+
+    return len(stack)
+
+#print(solution("()())()"))
+#print(solution("(()())()"))
+print(solution(")("))
