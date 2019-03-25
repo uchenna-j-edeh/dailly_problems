db21b52657d667983d82cf97265557be2a1ec35a
Uchenna J Edeh <write2uche@gmail.com>
2019-03-25 03:42:45 +0100

    updated square_outputs_sorted_order.py...

--- arrays_manipulations_algorithms/square_outputs_sorted_order.py
+++ arrays_manipulations_algorithms/square_outputs_sorted_order.py
@@ -5,6 +5,8 @@ steps:
 find the index of the lowest positive #.
 from there interate left and right comparing the results of both side and placing them in sorted order
 """
+import math
+
 def solution(my_list):
     # find the lowest positive, we could use binary search too
     lowest_positive = 0
@@ -12,10 +14,43 @@ def solution(my_list):
     for i, val in enumerate(my_list):
         if val > 0 and val < lowest_positive:
             lowest_positive = i
+    if i == 0:
+        return my_list
 
     new_list = []
-    for i in range(len(my_list)):
-        if 
+    k = 0
+    j = i - 1
+    while k < len(my_list):
+        if j < 0:
+            for u in range(i, len(my_list)):
+                new_list.append(math.square(u))
+            return
+        if i >= len(my_list):
+            for v in len(my_list):
+                if j - 1 < 0:
+                    return
+                new_list.append(math.square(j-1))
+
+        nsquare = math.square(my_list[i])
+        psquare = math.square(my_list[j])
+
+        if psquare < nsquare:
+            new_list.append(psquare)
+            i += 1
+        elif nsquare < psquare:
+            new_list.append(nsquare)
+            j -= 1
+        else:
+            new_list.append(psquare)
+            new_list.append(nsquare)
+            i += 1
+            j -= 1
+            k += 1
+
+        k += 1
+
+
+        
 
 
     
