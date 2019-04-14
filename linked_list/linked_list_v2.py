e8fae2d0793dcb74d6fa534e63cc2534b76820ef
Uchenna Edeh <write2uche@gmail.com>
2019-04-14 13:23:54 -0500

    addded more solution...

--- linked_list/linked_list_v2.py
+++ linked_list/linked_list_v2.py
@@ -43,9 +43,58 @@ class LinkedList:
 
             temp = temp.next_element    
 
+        return False    
+
+    def insert(self, value, insertion_point):
+        if self.head.next_element is None:
+            self.head.next_element = Node(value)
+
+        temp = self.head.next_element
+        while(temp.next_element != None):
+            if temp.data == insertion_point:
+                new_node = Node(value)
+                new_node.next_element = temp.next_element
+                temp.next_element = new_node
+                return True
+
+            temp = temp.next_element
+        
+        return False
+    
+    def delete(self, value):
+        if self.head.next_element == None:
+            return False
+
+        previous = None
+        temp = self.head.next_element
+        if temp.data == value: # when we only have one element to delete
+            self.deleteAtHead()
+
+        while(temp.next_element != None):
+            if temp.data == value:
+                previous.next_element = temp.next_element
+                return True
+
+            previous = temp
+            temp = temp.next_element
+
+        if temp.data == value:
+            previous.next_element = temp.next_element
+            return True
+
         return False
 
-            
+    def deleteAtHead(self):
+        if self.head.next_element is None:
+            return False
+
+        self.head.next_element = self.head.next_element.next_element
+        return True   
+
+    def append(self, value):
+        self.insertAtTail(value)
+        
+        
 
     def printList(self):
         if(self.isEmpty()):
@@ -62,20 +111,29 @@ class LinkedList:
  
 hlist = LinkedList()
 hlist.printList()
-
-for i in range(1, 100):
+count = 10
+for i in range(1, count):
     hlist.insertAtHead(i)
 
 hlist.printList()
-print(hlist.search(100))
+print(hlist.search(count))
+hlist.insert(90000, 8)
+hlist.printList()
+hlist.delete(90000)
+hlist.printList()
+hlist.append(0)
+hlist.printList()
+hlist.deleteAtHead()
+hlist.printList()
 
-tlist = LinkedList()
-tlist.printList()
 
-for i in range(1, 100):
-    tlist.insertAtTail(i)
+# tlist = LinkedList()
+# tlist.printList()
 
-tlist.printList()
-print(tlist.search(99))
-print(tlist.search(1))
+# for i in range(1, count):
+#     tlist.insertAtTail(i)
+
+# tlist.printList()
+# print(tlist.search(99))
+# print(tlist.search(1))
 
