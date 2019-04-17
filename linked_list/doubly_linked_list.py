34ce7b700345d9abd7cdf31bd4491df1bcfbea27
Uchenna J. Edeh <uchenna.j.edeh@jpmchase.com>
2019-04-16 19:14:00 -0500

    added dll...

---
+++ linked_list/doubly_linked_list.py
@@ -0,0 +1,11 @@
+
+class Node:
+    def __init__(self, value):
+        self.data = value #stores data
+        self.previous_element = None # Stores pointer to the previous element
+        self.next_element = None # Stores pointer to the next element
+
+
+    
+
+
