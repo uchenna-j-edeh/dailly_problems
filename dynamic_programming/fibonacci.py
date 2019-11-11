f574b9f1c74ba10added05174f091a74c054b5ee
Uchenna Edeh <uedeh@bethel.jw.org>
2019-11-11 11:59:43 -0600

    added new file...

---
+++ dynamic_programming/fibonacci.py
@@ -0,0 +1,7 @@
+# add the previous 2 sequence
+# if 0, return 0 + 0 = 0
+# if 1, return 1 + 0 = 1
+# if 2, return 1 + 0 = 1
+# if 3, return 2 + 1 = 2
+# if 4 return 2 + 1 = 3
+# if 5 return 3 + 2 = 5
